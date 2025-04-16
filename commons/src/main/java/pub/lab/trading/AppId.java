659a94337332ef4739ac83c11f691559b74b827f
Nitin Dandriyal <nitin.dandriyal@gmail.com>
2025-04-16 18:09:13 +0100

    initial

---
+++ commons/src/main/java/pub/lab/trading/AppId.java
@@ -0,0 +1,34 @@
+package pub.lab.trading;
+
+import org.agrona.collections.Int2ObjectHashMap;
+
+public enum AppId {
+    AERON_MEDIA_DRIVER(0),
+    CONFIG_SERVICE(1),
+    QUOTING_ENGINE(2),
+    PRICING_ENGINE(3),
+    MARKET_DATA(4),
+    STANDARD_ADAPTER(5);
+
+    private final int code;
+
+    AppId(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    private static final Int2ObjectHashMap<AppId> MAP = new Int2ObjectHashMap<>();
+
+    static {
+        for (AppId t : values()) {
+            MAP.put(t.code, t);
+        }
+    }
+
+    public static AppId fromCode(int code) {
+        return MAP.get(code);
+    }
+}
---
+++ pricing-engine/src/main/java/play/lab/pricing/engine/PricingEngineApp.java
@@ -0,0 +1,7 @@
+package play.lab.pricing.engine;
+
+public class PricingEngineApp {
+    public static void main(String[] args) {
+        new CoreEventLoop(5_000).runLoop();;
+    }
+}
